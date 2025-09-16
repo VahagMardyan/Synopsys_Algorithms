50f4d779373854e2aa3cc120d92985b9efb518dc
user.email <mardyan2003@gmail.com>
2025-09-16 18:07:45 +0400

    Homework 2 (merge sort)

--- main.py
+++ main.py
@@ -40,16 +40,74 @@ def format_auto_duration(d:float):
         return f"{ns / 1_000_000_000:.2f} s"
     
 
-def timing(sizes:list):
-
-    for n in sizes:
-        arr = [random.randint(0,10000) for _ in range(n)]
+def insertion_sort_timer(max_size:int = 10_000,factor:int = 10):
+    n = 10
+    while n<=max_size:
+        arr = [ random.randint(0,10000) for _ in range(n) ]
         start = time.time()
         insertion_sort(arr)
         end = time.time()
         duration = end - start
         print(f"Array size {n}: time={format_auto_duration(duration)}")
+        n += int((n * factor)/100)
 
-sizes = [100,200,500,1000,2000,5000,10000,20000,50000,100000,200000,500000]
 
-timing(sizes)
+def merge(arr:list, left:int, mid:int, right:int):
+    n1 = mid - left + 1
+    n2 = right - mid
+    L=[0] * n1
+    R=[0] * n2
+    for i in range(n1):
+        L[i] = arr[left + i]
+    
+    for j in range(n2):
+        R[j] = arr[mid + 1 + j]
+    
+    i=0
+    j=0
+    k=left
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i+=1
+        else:
+            arr[k] = R[j]
+            j+=1
+        k+=1
+
+    while i < n1:
+        arr[k] = L[i]
+        i+=1
+        k+=1
+
+    while j < n2:
+        arr[k] = R[j]
+        j+=1
+        k+=1
+    
+def merge_sort(arr:list, left:int, right:int):
+    if left < right:
+        mid = (left + right) // 2
+        merge_sort(arr, left, mid)
+        merge_sort(arr, mid+1, right)
+        merge(arr, left, mid, right)
+
+def merge_sort_timer(max_size:int = 10_000, factor:int = 10):
+    n = 10
+    while n<=max_size:
+        arr = [ random.randint(0,10000) for _ in range(n) ]
+        start = time.time()
+        merge_sort(arr, 0, len(arr) - 1)
+        end = time.time()
+        duration = end - start
+        print(f"Array size {n}: time={format_auto_duration(duration)}")
+        n += int((n * factor)/100)
+
+m_size = 1000
+
+print("Insertion sort results:\n")
+insertion_sort_timer(m_size)
+
+print("\nMerge sort results:\n")
+merge_sort_timer(m_size)
