f113138c97891260915eba0a390ab34896e5e213
user.email <mardyan2003@gmail.com>
2025-09-17 11:01:34 +0400

    Modified

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-# # Insertion Sort
+# # Insertion Sort algorithm
 
 def insertion_sort(arr:list):
     for i in range(1,arr.__len__()):
@@ -11,46 +11,6 @@ def insertion_sort(arr:list):
             arr[j+1] = arr[j]
             j=j-1
         arr[j+1] = key
-
-a = [1,5,66,12] # # 1 5 12 66
-
-# insertion_sort(a)
-
-# print(a)
-
-def generate_array(a:int, b:int, size:int):
-    arr=[]
-    for n in range(0,size):
-        arr.append(random.randint(a,b))
-    insertion_sort(arr)
-    return arr
-
-array = generate_array(1,1000,12)
-# print(array)
-
-def format_auto_duration(d:float):
-    ns = d * 1e9
-    if ns < 1_000:
-        return f"{ns:.2f} ns"
-    elif ns < 1_000_000:
-        return f"{ns / 1_000:.2f} µs" # # microseconds
-    elif ns < 1_000_000_000:
-        return f"{ns / 1_000_000:.2f} ms"
-    else:
-        return f"{ns / 1_000_000_000:.2f} s"
-    
-
-def insertion_sort_timer(max_size:int = 10_000,factor:int = 10):
-    n = 10
-    while n<=max_size:
-        arr = [ random.randint(0,10000) for _ in range(n) ]
-        start = time.time()
-        insertion_sort(arr)
-        end = time.time()
-        duration = end - start
-        print(f"Array size {n}: time={format_auto_duration(duration)}")
-        n += int((n * factor)/100)
-
 
 def merge(arr:list, left:int, mid:int, right:int):
     n1 = mid - left + 1
@@ -92,22 +52,14 @@ def merge_sort(arr:list, left:int, right:int):
         merge_sort(arr, left, mid)
         merge_sort(arr, mid+1, right)
         merge(arr, left, mid, right)
-
-def merge_sort_timer(max_size:int = 10_000, factor:int = 10):
-    n = 10
-    while n<=max_size:
-        arr = [ random.randint(0,10000) for _ in range(n) ]
-        start = time.time()
-        merge_sort(arr, 0, len(arr) - 1)
-        end = time.time()
-        duration = end - start
-        print(f"Array size {n}: time={format_auto_duration(duration)}")
-        n += (n * factor)//100
-
-m_size = 1000
-
-print("Insertion sort results:\n")
-insertion_sort_timer(m_size)
-
-print("\nMerge sort results:\n")
-merge_sort_timer(m_size)
+ 
+# def format_auto_duration(d:float):
+#     ns = d * 1e9
+#     if ns < 1_000:
+#         return f"{ns:.2f} ns"
+#     elif ns < 1_000_000:
+#         return f"{ns / 1_000:.2f} µs" # # microseconds
+#     elif ns < 1_000_000_000:
+#         return f"{ns / 1_000_000:.2f} ms"
+#     else:
+#         return f"{ns / 1_000_000_000:.2f} s"
---
+++ test.py
@@ -0,0 +1,82 @@
+from main import *
+import random, time
+import csv
+
+
+
+def generate_list(n:int, min_val:int = 0, max_val:int = 10_000) -> list:
+    return [ random.randint(min_val, max_val) for _ in range(n) ]
+
+def is_sorted(seq):
+    """ Check if sequence is sorted """
+    return all(seq[i] <= seq[i+1] for i in range(len(seq)-1))
+
+class SortingTester:
+    def __init__(self, sorting_func):
+        self.sorting_func = sorting_func
+        self.results = []
+    
+    def test_correctness_once(self, n):
+        """Generate random list, sort it, check correctness"""
+        arr = generate_list(n)
+        original = arr[:]
+        self.sorting_func(arr)
+        if not is_sorted(arr):
+            print("Not sorted properly")
+            return False
+        
+        if sorted(original) != arr:
+            print("Elements missing or duplicated")
+            return False
+        return True
+    
+    def test_correctness(self, factor=20, limit=10_000):
+        """Run correctness tests with increasing sizes"""
+        count = 10
+        while count < limit:
+            if self.test_correctness_once(count):
+                print(f"Correctness test passed with {count} elements")
+            else:
+                print(f"Correctness test failed with {count} elements")
+                return False
+            count += (count * factor) // 100
+        return True
+    
+    def measure_time(self, n):
+        """Measure time spent on sorting"""
+        arr = generate_list(n)
+        start = time.perf_counter()
+        self.sorting_func(arr)
+        end = time.perf_counter()
+        spent_ms = (end - start) * 1000
+        self.results.append((n,spent_ms))
+        print(f"Sorting {n} elements took {spent_ms:.2f} ms")
+    
+    def test_wall_time(self, factor=10, start_size=10, limit=10_00):
+        """Run performance tests with increasing sizes"""
+        count = start_size
+        while count < limit:
+            self.measure_time(count)
+            count += (count * factor) // 100
+        return True
+    
+    def export_results(self,filename="sorting_times.xlsx"):
+        with open("sorting_times.csv", "w", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(["Size", "Time_ms"])
+            writer.writerows(self.results)
+        print("Exported sorting times to sorting_times.csv")
+    
+
+def merge_sort_wrapper(arr):
+    """Wrapper so we can pass merge_sort to SortingTester"""
+    if arr:
+        merge_sort(arr, 0, len(arr)-1)
+
+def run_sorting_test(test_name="",sorting_func=None):
+    tester = SortingTester(sorting_func)
+    tester.test_correctness()
+    tester.test_wall_time()
+    # tester.export_results(f"{test_name.replace(' ','_')}_data.csv")
+
+run_sorting_test("Insertion sort",insertion_sort)
