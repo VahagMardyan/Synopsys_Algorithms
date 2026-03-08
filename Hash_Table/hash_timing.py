3650678993ae1030c4d73fd68621cb7859cc8daf
VahagMardyan <mardyan2003@gmail.com>
2026-03-08 13:57:55 +0400

    Little changes into test-file

--- Hash_Table/hash_timing.py
+++ Hash_Table/hash_timing.py
@@ -2,15 +2,15 @@ import time
 from hash_table import Hash_Table
 
 data_size = 10000
-keys = [f"key_{i}" for i in range(data_size)]
-values = [i for i in range(data_size)]
+keys = [i for i in range(data_size)]
+values = [i**2 for i in range(data_size)]
 
 ht = Hash_Table(100)
 
 for i in range(data_size):
     ht.insert(keys[i], values[i])
 
-target_key = "key_9999"
+target_key = 9999
 
 iterations = 10000
 
@@ -22,6 +22,6 @@ ht_time = time.perf_counter() - start_time
 # print(f"Total time for {iterations} searches: {ht_time:.6f} seconds")
 # print(f"Average time for per search is: {(ht_time / iterations):.10f} seconds")
 
-ht.get_stats()
+# ht.get_stats()
 # print()
-# print(ht)
+print(ht)
