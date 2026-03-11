1f3e3a3e3c772a420d8c6295c93375469984361a
VahagMardyan <mardyan2003@gmail.com>
2026-03-11 10:17:00 +0400

    Counting Sort

---
+++ Sorting_Algorithms/counting_sort.py
@@ -0,0 +1,25 @@
+def countsort(arr:list) -> list:
+    if not arr:
+        return []
+    
+    n = len(arr)
+    max_val = max(arr)
+    cnt_arr = [0] * (max_val + 1)
+    for v in arr:
+        cnt_arr[v] += 1
+    
+    for i in range(1, max_val + 1):
+        cnt_arr[i] += cnt_arr[i - 1]
+    
+    ans = [0] * n
+
+    for i in range(n - 1, -1, -1):
+        v = arr[i]
+        ans[cnt_arr[v] - 1] = v
+        cnt_arr[v] -= 1
+
+    return ans
+
+arr = [2, 5, 3, 0, 2, 2, 3, 0, 3]
+ans = countsort(arr)
+print(ans)
